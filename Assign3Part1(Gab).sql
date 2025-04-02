20779ecf77671956633ab26b4fc4cf4c9f167985
GabrielSiwa <gabrielirasiwa.8@gmail.com>
2025-04-01 23:27:19 -0600

    Works without errors. (Please double check)
    Found out that the error is the order of the transaction_detail and history because detail table has a foreign key from history which is the transaction number
    
    ORA-02291: integrity constraint (SYSTEM.FK_TD_TO_TRANSACTION) violated - parent key not found
    ORA-06512: at line 74

---
+++ Assign3Part1(Gab).sql
@@ -0,0 +1,107 @@
+declare
+    -- Outer cursor to fetch distinct transaction numbers
+   cursor trans_cursor is
+   select distinct transaction_no
+     from new_transactions;
+    
+    -- Inner cursor to fetch transaction-level data for a specific transaction number
+   cursor trans_details_cursor (
+      p_transaction_number new_transactions.transaction_no%type
+   ) is
+   select transaction_date,
+          description,
+          account_no,
+          transaction_type,
+          transaction_amount
+     from new_transactions
+    where transaction_no = p_transaction_number;
+
+    -- Cursor to fetch default transaction type from ACCOUNT_TYPE
+    cursor account_type_cursor (p_account_no account.account_no%type) is
+    select at.default_trans_type
+        from account a
+        join account_type at on a.account_type_code = at.account_type_code
+        where a.account_no = p_account_no;
+
+    v_default_trans_type account_type.default_trans_type%type;
+    v_transaction_date DATE; --added by gab
+    v_description VARCHAR2(100); -- added by gab  
+
+begin 
+    -- Outer loop: Loop through each distinct transaction number
+   for trans_rec in trans_cursor loop
+   
+        -- Inner loop: Loop through each row for the current transaction number
+      for trans_details_rec in trans_details_cursor(trans_rec.transaction_no) loop
+      
+      v_transaction_date := trans_details_rec.transaction_date; -- added by gab
+      v_description := trans_details_rec.description; -- added by gabZ
+        -- Fetch the default transaction type
+        open account_type_cursor(trans_details_rec.account_no);
+        fetch account_type_cursor into v_default_trans_type;
+        close account_type_cursor;
+        
+        -- Update ACCOUNT balance based on transaction type
+        -- If default transaction type is 'D' (Debit Account)
+        if v_default_trans_type = 'D' then
+            -- Add amount if it's a Debit transaction
+            if trans_details_rec.transaction_type = 'D' then
+                update account
+                set account_balance = account_balance + trans_details_rec.transaction_amount
+                where account_no = trans_details_rec.account_no;
+            else
+             -- Subtract amount if it's a Credit transaction
+                update account
+                set account_balance = account_balance - trans_details_rec.transaction_amount
+                where account_no = trans_details_rec.account_no;
+            end if;
+        else -- If default transaction type is 'C' (Credit Account)
+            if trans_details_rec.transaction_type = 'D' then
+            -- Subtract amount if it's a Debit transaction
+                update account
+                set account_balance = account_balance - trans_details_rec.transaction_amount
+                where account_no = trans_details_rec.account_no;
+            else
+            -- Add amount if it's a Credit transaction
+                update account
+                set account_balance = account_balance + trans_details_rec.transaction_amount
+                where account_no = trans_details_rec.account_no;
+            end if;
+        end if;
+
+      end loop; -- inserted 04012025 Bhel
+        
+        -- Insert into TRANSACTION_HISTORY
+       insert into transaction_history (
+            transaction_no,
+            transaction_date,
+            description
+        ) values (
+            trans_rec.transaction_no,
+            v_transaction_date, -- changed by gab
+            v_description  -- changed by gab
+        );
+        
+        for trans_details_rec in trans_details_cursor(trans_rec.transaction_no) loop
+            -- Insert into TRANSACTION_DETAIL
+         insert into transaction_detail ( --Moved by gab
+            account_no,
+            transaction_no,
+            transaction_type,
+            transaction_amount
+         ) values ( trans_details_rec.account_no,
+                    trans_rec.transaction_no,
+                    trans_details_rec.transaction_type,
+                    trans_details_rec.transaction_amount );
+                    
+         end loop;
+
+        -- Delete processed rows from NEW_TRANSACTIONS 
+      delete from new_transactions
+       where transaction_no = trans_rec.transaction_no;
+
+   end loop;
+
+   commit;
+end;
+/
